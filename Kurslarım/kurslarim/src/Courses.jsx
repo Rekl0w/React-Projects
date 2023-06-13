60becb45fa014659976ab8b0b4960df8c1c90fb2
Batuhan Tomo <batuhan.tomo@gmail.com>
2023-06-13 10:49:07 +0300

    Update

--- Kurslarım/kurslarim/src/Courses.jsx
+++ Kurslarım/kurslarim/src/Courses.jsx
@@ -1,23 +1,57 @@
-import Course from "./Course"
+import Course from "./Course";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
-function Courses({courses , removeCourse}) {
+function Courses({ courses }) {
+  const [index, setIndex] = useState(0);
+  const { content, title, price } = courses[index];
 
-    return (<div className="courseMainDiv">
-        <div>
-            <h2>Kurslarım</h2>
-        </div>
-        <div className="cardDiv">
-            {
-                courses.map((course) => {
-                    return(
-                        <Course key={course.id} {...course} removeOneCourse={removeCourse}/>
-                    )   
-                })
+  const randomCourse = () => {
+    let randomIndex = Math.floor(Math.random() * courses.length);
+
+    if (randomIndex === index) {
+      randomIndex = index + 1;
+    }
+
+    setIndex(randomIndex);
+  };
+
+  return (
+    <div className="courseMainDiv">
+      <div className="courseTitleandButton">
+        <h2>Kurslarım</h2>
+        <button className="cardDeleteBtn" onClick={randomCourse}>
+          {" "}
+          Rastgele Kurs Ata!
+        </button>
+      </div>
+
+      <div className="cardDiv">
+        <button className="prevNextBtn">
+          <FaChevronLeft
+            onClick={() =>
+              index !== 0 ? setIndex(index - 1) : setIndex(courses.length - 1)
             }
+          />
+        </button>
 
+        <div className="card">
+          <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <h4 className="cardPrice">{price}TL</h4>
+          </div>
+          <p>{content}</p>
         </div>
-    </div>)
-    
+        <button className="prevNextBtn">
+          <FaChevronRight
+            onClick={() =>
+              index !== courses.length - 1 ? setIndex(index + 1) : setIndex(0)
+            }
+          />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Courses
+export default Courses;
